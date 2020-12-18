4d3c1d0be146d89218cf16d0cb28237cf92f3923
harshitagarwal260 <64749417+harshitagarwal260@users.noreply.github.com>
2020-12-18 13:42:07 +0530

    Add files via upload

---
+++ enroll.py
@@ -0,0 +1,19 @@
+from cousre import Cousre
+from student import Student
+from datetime import datetime
+
+class Enroll:
+	def __init__(self, student, course):
+		if not isinstance(student, Student):
+			raise Error("Invalid student...")
+
+		if not isinstance(course, Cousre):
+			raise Error("Invalid course...")
+
+		self.student = student
+		self.course = course
+		self.grade = None
+		self.date = datetime.now()
+
+	def set_grade(self, grade):
+		self.grade = grade
